f32371c0cee84e6bcc411919a6b75a5b5c3715c9
Duncan Mac-Vicar P. <duncan@mac-vicar.eu>
2023-11-19 17:48:16 +0100

    format code as gtk

--- src/appmenu-ng-gtk-module.c
+++ src/appmenu-ng-gtk-module.c
@@ -24,36 +24,47 @@
 
 static guint signal_id_widget_window_state_event;
 
-static gboolean emission_hook_cb(GSignalInvocationHint *hint, guint n_param_values, const GValue *param_values, gpointer data) {
+static gboolean
+emission_hook_cb (GSignalInvocationHint *hint, guint n_param_values, const GValue *param_values, gpointer data)
+{
   GObject *object;
-  object = g_value_get_object(&param_values[0]);
+  object = g_value_get_object (&param_values[0]);
 
-  if (!GTK_IS_WINDOW(object)) {
-	return TRUE;
-  }
-  
-  if (hint->signal_id == signal_id_widget_window_state_event) {
-	GdkEventWindowState *e  = (GdkEventWindowState *) gdk_event_copy(g_value_peek_pointer(&param_values[1]));
-	if (e->new_window_state & GDK_WINDOW_STATE_FOCUSED) {
-	  g_message("window '%s' is now focused", gtk_window_get_title(GTK_WINDOW(object)));
-	} else {
-	  g_message("window '%s' is not focused", gtk_window_get_title(GTK_WINDOW(object)));
-	}
-  }
+  if (!GTK_IS_WINDOW (object))
+    {
+      return TRUE;
+    }
+
+  if (hint->signal_id == signal_id_widget_window_state_event)
+    {
+      GdkEventWindowState *e = (GdkEventWindowState *) gdk_event_copy (g_value_peek_pointer (&param_values[1]));
+      if (e->new_window_state & GDK_WINDOW_STATE_FOCUSED)
+        {
+          g_message ("window '%s' is now focused", gtk_window_get_title (GTK_WINDOW (object)));
+        }
+      else
+        {
+          g_message ("window '%s' is not focused", gtk_window_get_title (GTK_WINDOW (object)));
+        }
+    }
   return TRUE;
 }
 
-static void install_hook(GType type, const char *sig, guint *sn) {
+static void
+install_hook (GType type, const char *sig, guint *sn)
+{
   GTypeClass *type_class;
 
-  type_class = g_type_class_ref(type);
+  type_class = g_type_class_ref (type);
 
-  *sn = g_signal_lookup(sig, type);
-  g_signal_add_emission_hook(*sn, 0, emission_hook_cb, NULL, NULL);
+  *sn = g_signal_lookup (sig, type);
+  g_signal_add_emission_hook (*sn, 0, emission_hook_cb, NULL, NULL);
 
-  g_type_class_unref(type_class);
+  g_type_class_unref (type_class);
 }
 
-G_MODULE_EXPORT void gtk_module_init(gint *argc, gchar ***argv[]) {
-  install_hook(GTK_TYPE_WIDGET, "window-state-event", &signal_id_widget_window_state_event);
+G_MODULE_EXPORT void
+gtk_module_init (gint *argc, gchar ***argv[])
+{
+  install_hook (GTK_TYPE_WIDGET, "window-state-event", &signal_id_widget_window_state_event);
 }
